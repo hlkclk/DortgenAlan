9b0070aa84e9330fba4eb9fbe2e22dad1c94ba8a
haluk <haluk@DESKTOP-1ED8B70>
2024-01-14 21:54:00 +0300

    AlanHesapla

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class AlanHesapla {
+public:
+	float genislik;
+	float yukseklik;
+	float alan;
+	
+
+void giris (){
+	cout << "yukseklik giriniz"<< endl;
+cin >> yukseklik;
+cout << "genislik giriniz" << endl;
+cin >> genislik;}
+void hesapla () {
+	alan = yukseklik * genislik;
+	cout <<" Alan = "<< alan;
+
+
+}
+};
+
+
+int main()
+{
+	while (true)
+	{
+
+		cout << "\n***hesap***\n";
+	AlanHesapla hesap;
+	hesap.giris();
+	hesap.hesapla();
+}
+}
+
+
